61a9020a25c96ee1caf5e34bad9b1905a70c4cdc
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-26 10:45:50 -0600

    entire show model

--- Tour-Ready-Capstone/Models/Show.cs
+++ Tour-Ready-Capstone/Models/Show.cs
@@ -5,6 +5,15 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int GroupId { get; set; }
+        public string Venue { get; set; }
+        public DateTime ShowDate { get; set; }
+        public int CityId { get; set; }
+        public string SetList { get; set; }
+        public string ShowNotes { get; set; }
+        public int MerchSales { get; set; }
+        public int Payout { get; set; }
+        public Boolean IsFavorite { get; set; }
+
 
 
     }
