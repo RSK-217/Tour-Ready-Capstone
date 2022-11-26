9808f4c5e3355bc7144d46bb4f4ee40db1a379ff
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-26 10:41:46 -0600

    properties in show

---
+++ Tour-Ready-Capstone/Models/Show.cs
@@ -0,0 +1,11 @@
+﻿namespace Tour_Ready_Capstone.Models
+{
+    public class Show
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int GroupId { get; set; }
+
+
+    }
+}
