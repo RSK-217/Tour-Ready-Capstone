50df0238b86bf5fa3642e10760b5ca99597707ef
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-28 20:11:39 -0600

    groupMember interface and controller

---
+++ Tour-Ready-Capstone/Controllers/GroupMemberController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tour_Ready_Capstone.Models;
+using Tour_Ready_Capstone.Repositories;
+using Tour_Ready_Capstone.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tour_Ready_Capstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupMemberController : ControllerBase
+    {
+        public GroupMemberController(
+    IGroupMember groupMemberRepository)
+
+        {
+            _groupMemberRepo = groupMemberRepository;
+
+        }
+        // GET: api/<GroupMemberController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GroupMemberController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GroupMemberController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GroupMemberController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GroupMemberController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Tour-Ready-Capstone/Interfaces/IGroupMember.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tour_Ready_Capstone.Models;
+
+namespace Tour_Ready_Capstone.Interfaces
+{
+    public interface IGroupMember
+    {
+    }
+}
