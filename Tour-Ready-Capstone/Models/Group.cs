202cb78a8495d95d6296cda7ae08299d1492dacb
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-28 19:23:07 -0600

    group model

---
+++ Tour-Ready-Capstone/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace Tour_Ready_Capstone.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Image { get; set; }
+
+    }
+}
