24adc3afa24ff3410078269e036b210277089deb
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-29 19:40:04 -0600

    City model, "cityName" database fix

---
+++ Tour-Ready-Capstone/Models/City.cs
@@ -0,0 +1,14 @@
+﻿namespace Tour_Ready_Capstone.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } 
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string CityNotes { get; set; }
+
+
+    }
+}
