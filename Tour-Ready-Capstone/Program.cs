c288d474f49bf693846534d96e74e55779f54fa4
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-30 22:11:30 -0600

    program cs addditions (cors, jwtbearer)

--- Tour-Ready-Capstone/Program.cs
+++ Tour-Ready-Capstone/Program.cs
@@ -1,19 +1,55 @@
 using Tour_Ready_Capstone.Interfaces;
 using Tour_Ready_Capstone.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+//FirebaseApp.Create(new AppOptions
+//{
+//    Credential = GoogleCredential.FromFile("")
+//});
+
+var firebaseProjectId = builder.Configuration.GetValue<string>("Authentication:Firebase:ProjectId");
+var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = googleTokenUrl;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = googleTokenUrl,
+            ValidateAudience = true,
+            ValidAudience = firebaseProjectId,
+            ValidateLifetime = true
+        };
+    });
 // Add services to the container.
 builder.Services.AddTransient<IUser, UserRepository>();
 builder.Services.AddTransient<IGroup, GroupRepository>();
 builder.Services.AddTransient<IGroupMember, GroupMemberRepository>();
 builder.Services.AddTransient<IShow, ShowRepository>();
 builder.Services.AddTransient<ICity, CityRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 
 var app = builder.Build();
 
@@ -25,7 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
