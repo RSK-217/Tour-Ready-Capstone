d6e21902185d5248628af417f8918f52b902de32
Rick Kersmarki <rkersmarki@gmail.com>
2023-01-17 21:26:47 -0600

    property fixes(temporary)

--- Tour-Ready-Capstone/Models/City.cs
+++ Tour-Ready-Capstone/Models/City.cs
@@ -7,9 +7,6 @@
         public string CityName { get; set; }
         public string State { get; set; }
         public string Country { get; set; }
-        public  string People { get; set; }
-        public string Places { get; set; }
-        public string CityNotes { get; set; }
 
 
     }
