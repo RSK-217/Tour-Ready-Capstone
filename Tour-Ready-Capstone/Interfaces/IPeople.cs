bbef94b91565f297f7a28c616da38714f51a2833
Rick Kersmarki <rkersmarki@gmail.com>
2023-01-17 23:11:32 -0600

    People complete

---
+++ Tour-Ready-Capstone/Interfaces/IPeople.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tour_Ready_Capstone.Models;
+
+namespace Tour_Ready_Capstone.Interfaces
+{
+    public interface IPeople
+    {
+        public People GetPersonById(int id);
+        public List<People> GetAllPeopleByCityId(int id);
+        public People CreatePerson(People people);
+        public void UpdatePerson(People people);
+        public void DeletePerson(int id);
+
+    }
+}
