13ab426eccc94cb61844b585b95dd47d068583e3
Rick Kersmarki <rkersmarki@gmail.com>
2023-01-25 15:26:13 -0600

    city view save

--- Tour-Ready-Capstone/Models/CityViewModel.cs
+++ Tour-Ready-Capstone/Models/CityViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class CityViewModel
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string? Person { get; set; }
+        public string? PlaceName { get; set; }
+        public string? Note { get; set; }
+
+
     }
 }
